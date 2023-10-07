ff50e7a3daefe328690d37a3515a1c7f94c09c91
Estelle <estelle.baby3@etu.univ-lorraine.fr>
2023-10-07 18:36:52 +0200

    plus d'erreurs basic auth

--- demo/src/main/java/com/example/demo/VaccinationCenter/UtilisateurService.java
+++ demo/src/main/java/com/example/demo/VaccinationCenter/UtilisateurService.java
@@ -5,9 +5,16 @@ import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import jakarta.annotation.PostConstruct;
+// import com.example.demo.VaccinationCenter.UtilisateurRepository;
+// import com.example.demo.VaccinationCenter.Utilisateur;
 
 @Service
 public class UtilisateurService implements UserDetailsService {
