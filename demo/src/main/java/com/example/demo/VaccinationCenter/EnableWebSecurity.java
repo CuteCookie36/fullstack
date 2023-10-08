d67015295f765984c1a985ca61345e43917ef3d8
Miist3rNyz <kevinkesteloot76@gmail.com>
2023-10-07 20:05:34 -0400

    suppression authentification car bug

--- demo/src/main/java/com/example/demo/VaccinationCenter/EnableWebSecurity.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo.VaccinationCenter;
-
-public @interface EnableWebSecurity {
-
-}
--- demo/src/main/java/com/example/demo/VaccinationCenter/UtilisateurRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.VaccinationCenter;
-
-import java.util.Optional;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UtilisateurRepository extends JpaRepository<Utilisateur, Long> {
-    Optional<Utilisateur> findByLogin(final String email);
-}
