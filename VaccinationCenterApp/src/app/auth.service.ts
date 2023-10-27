b87a8205af467610fd3eb157ff3aa79bff49913a
Estelle <estelle.baby3@etu.univ-lorraine.fr>
2023-10-27 19:42:28 +0200

    suite_auth

--- VaccinationCenterApp/src/app/auth.service.ts
+++ VaccinationCenterApp/src/app/auth.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Utilisateur } from './utilisateur';
+import { shareReplay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -19,6 +21,13 @@ export class AuthService {
     const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
 
     return this.http.post(`${this.apiUrl}/auth`, body, { headers: headers });
+  }
+
+  login(login:string, password:string ) {
+    return this.http.post<Utilisateur>('/api/admin/login', {login, password})
+        // this is just the HTTP call, 
+        // we still need to handle the reception of the token
+        //.shareReplay();
   }
 
 
--- VaccinationCenterApp/src/app/login/login.component.ts
+++ VaccinationCenterApp/src/app/login/login.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +10,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  form:FormGroup;
+  
+  constructor(private fb:FormBuilder, private authService: AuthService, private router: Router) {
 
+    this.form = this.fb.group({
+      login: ['',Validators.required],
+      password: ['',Validators.required]
+    });
+  }
+
+  login() {
+    const val = this.form.value;
+    console.log("appui");
+    if (val.login && val.password) {
+        this.authService.login(val.login, val.password)
+            .subscribe(
+                () => {
+                    console.log("User is logged in");
+                    this.router.navigateByUrl('/centers');
+                }
+            );
+    }
+  }
+  
 }
